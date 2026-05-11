c264848f0483bfc641f53bdeeaa724a5d6d90068
Alice Till <alice@bellridgemg.com>
2026-05-11 13:06:10 -0400

    setup get route

--- app/api/setup/route.js
+++ app/api/setup/route.js
@@ -3,6 +3,11 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
+export async function GET() {
+  await setupDB();
+  return NextResponse.json({ ok: true });
+}
+
 export async function POST() {
   await setupDB();
   return NextResponse.json({ ok: true });
